89a2b08556fbc4eff05f8fa310764ad478de4a05
LUCA <luca.cirone94@gmail.com>
2022-07-23 17:20:31 +0900

    secondrun

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+  get "calendar/index", to:"calendar#index"
+  get "oauth2callback", to:"calendar#callback"
   resources :events
   devise_for :users
   get "about-us", to: "about#index", as: :about
 
-  get "calendar", to: "calendar#index"
+  get "calendar/index", to:"calendar#index"
+  get "oauth2callback", to:"calendar#callback"
 
   get "timezone", to: "timezone#index"
 
