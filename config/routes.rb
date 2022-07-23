46d6b5251e577603e38bbcc55eeab555005fa9c4
LuxHollow <luca.cirone94@gmail.com>
2022-07-23 17:46:04 +0900

    indexback

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  get "calendar/index", to:"calendar#index"
   get "oauth2callback", to:"calendar#callback"
   resources :events
   devise_for :users
   get "about-us", to: "about#index", as: :about
 
-  get "calendar/index", to:"calendar#index"
+  get "calendar", to:"calendar#index"
   get "oauth2callback", to:"calendar#callback"
 
   get "timezone", to: "timezone#index"
